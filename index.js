20e3a65f24991fc2e59e3a67cfd179902af5e1d3
0xflotus <0xflotus@gmail.com>
2020-12-20 23:24:31 +0100

    ES6 Upgrade

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-var leftpad = require('leftpad');
+module.exports = function (options) {
+  const showHours = options && !!options.showHours;
 
-module.exports = function(options) {
-  var showHours = options && !!options.showHours;
+  const leftpad = (what, length = 2) => what.toString().padStart(length, '0');
 
-  return function(seconds) {
-    var secs = Math.floor(seconds) % 60;
-    var millis = Math.round(seconds * 1000) % 1000;
-    var mins = Math.floor(seconds / 60);
-    var hours;
-    var str = '';
+  return function (seconds) {
+    console.assert(typeof seconds === 'number', `'seconds' must be of type number`);
+    console.assert(seconds >= 0, `'seconds' must be greater or equal 0`);
+    let secs = Math.floor(seconds) % 60;
+    let millis = Math.round(seconds * 1000) % 1000;
+    let mins = Math.floor(seconds / 60);
+    let hours;
+    let str = '';
     if (showHours) {
       hours = Math.floor(mins / 60);
       mins = mins % 60;
-      str += leftpad(hours, 2, 0) + ':';
+      str = `${leftpad(hours)}:`;
     }
-    return str + leftpad(mins, 2, 0) + ':' + leftpad(secs, 2, 0) + '.' + leftpad(millis, 3, 0);
+    return `${str}${leftpad(mins)}:${leftpad(secs)}.${leftpad(millis, 3)}`;
   }
 };
--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 var test = require('tap').test;
 var SixtySixty = require('..');
 
-test('formatting without hour component', function(t) {
+test('formatting without hour component', function (t) {
   var ss = SixtySixty();
+  console.log(ss(0), ss(-1), ss(Infinity), ss(''), ss(NaN), ss());
   t.equal(ss(0), '00:00.000');
   t.equal(ss(60), '01:00.000');
   t.equal(ss(666.6666), '11:06.667');
@@ -10,8 +11,8 @@ test('formatting without hour component', function(t) {
   t.end();
 });
 
-test('formatting with hour component', function(t) {
-  var ss = SixtySixty({showHours: true});
+test('formatting with hour component', function (t) {
+  var ss = SixtySixty({ showHours: true });
   t.equal(ss(0), '00:00:00.000');
   t.equal(ss(60), '00:01:00.000');
   t.equal(ss(666.6666), '00:11:06.667');
